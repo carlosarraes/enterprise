9716bc385f3de82af44db006caf32bc3a042838a
carraes <carraeshb@gmail.com>
2023-04-27 09:40:27 -0300

    feat: setup db connection

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using enterprise.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<EnterpriseDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DbConnection"));
+});
 
 var app = builder.Build();
 
