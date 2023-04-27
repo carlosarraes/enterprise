427229b9a19e467b2f73514858202395a926bd8a
carraes <carraeshb@gmail.com>
2023-04-27 11:18:43 -0300

    feat: updates ondelete migration

---
+++ Migrations/20230427141747_UpdateOnDelete.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace enterprise.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateOnDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
