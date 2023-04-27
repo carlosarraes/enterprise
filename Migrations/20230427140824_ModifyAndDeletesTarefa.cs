6abaf741d6ee12149e059b9a8fb8be0f0001637e
carraes <carraeshb@gmail.com>
2023-04-27 11:18:33 -0300

    feat: change string to bool on migration

---
+++ Migrations/20230427140824_ModifyAndDeletesTarefa.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace enterprise.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyDeletesAndTarefa : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "ALTER TABLE \"Tarefas\" ALTER COLUMN \"Status\" TYPE boolean USING \"Status\"::boolean"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "ALTER TABLE \"Tarefas\" ALTER COLUMN \"Status\" TYPE text USING \"Status\"::text"
+            );
+        }
+    }
+}
