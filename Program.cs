40e94fbafc6787554331e28337edea657474d6d1
carraes <carraeshb@gmail.com>
2023-04-28 01:12:26 -0300

    chore: setup swagger in prod

--- Program.cs
+++ Program.cs
@@ -34,12 +34,8 @@ builder.WebHost.ConfigureKestrel(serverOptions =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
